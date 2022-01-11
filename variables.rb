ad5bf765f088d7ee9c31bf843659e5b409d2ff9e
Susan Boyd <sueboyd922@gmail.com>
2022-01-10 22:20:24 -0500

    Reassign variables

--- variables.rb
+++ variables.rb
@@ -2,10 +2,16 @@ first_country = "Ireland"
 second_country = "Canada"
 fave_country = "Nepal"
 
+# assign number of countries in world and personal number
 number_of_countries = 195
 my_countries_num = 36
 
+# percentage of countries visited
 percent_of_countries = 0.18
 
 want_to_travel = true
 love_covid = false
+
+first_country = "France"
+second_country = "Thailand"
+fave_country = "Rwanda"
